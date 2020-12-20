7d1e13bac64ae8f11f34f04f237133e3e41044d2
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-12-20 22:35:51 +0100

    add routes util

---
+++ src/utils/routes.js
@@ -0,0 +1,15 @@
+const routes = [
+    {
+        menuName: "Generative Art",
+        baseUrl: "/portfolio",
+        pageTitle: "Generative Artist and Creative Coder",
+    },
+    { menuName: "Contact", baseUrl: "/contact", pageTitle: "About Me" },
+    {
+        menuName: "Home",
+        baseUrl: "/",
+        pageTitle: "Generative Artist and Creative Coder",
+    },
+]
+
+export default routes
