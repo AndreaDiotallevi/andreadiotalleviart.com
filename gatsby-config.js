bb403897988b56fb66a5c7929c5700c1c7bc64c1
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-12-20 22:33:22 +0100

    use graphQL to query artworks data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,59 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+require("dotenv").config({
+	path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+	siteMetadata: {
+		title: "Andrea Diotallevi | Generative Artist and Creative Coder",
+		author: "Andrea Diotallevi",
+		description: "The artwork of Andrea Diotallevi, a practising generative artist, creative coder, software engineer, architect and pianist.",
+		url: "https://www.andreadiotallevi.com",
+		image: "/moonlight.png",
+		twitterUsername: "@a_diotallevi_"
+	},
+	plugins: [
+		"gatsby-plugin-react-helmet",
+		"gatsby-plugin-sass",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "src",
+				path: `${__dirname}/src/`,
+			}
+		},
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		"gatsby-transformer-json",
+		{
+			resolve: "gatsby-transformer-remark",
+			options: {
+				plugins: [
+					"gatsby-remark-reading-time",
+					"gatsby-remark-relative-images",
+					{
+						resolve: "gatsby-remark-images",
+						options: {
+							maxWidth: 750,
+							linkImagesToOriginal: false
+						}
+					}
+				]
+			}
+		},
+		{
+			resolve: `gatsby-plugin-google-fonts`,
+			options: {
+				fonts: [
+					'Work Sans',
+				],
+				display: 'swap'
+			}
+		},
+		{
+			resolve: `gatsby-plugin-google-analytics`,
+			options: {
+				trackingId: process.env.GA_TRACKING_ID
+			}
+		}
+	]
 }
