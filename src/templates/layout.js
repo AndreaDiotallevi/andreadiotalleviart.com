3a5d39d1187fa07f3ae966dee3f118372ae4c045
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2021-09-17 22:31:50 +0100

    check if in browser before using window object

--- src/templates/layout.js
+++ src/templates/layout.js
@@ -8,11 +8,15 @@ import Footer from "../components/footer"
 
 const Layout = ({ children }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const isBrowser = typeof window !== "undefined"
 
   return (
     <Location>
       {({ location }) => (
-        <div className="app" style={{ minHeight: window.innerHeight }}>
+        <div
+          className="app"
+          style={{ minHeight: isBrowser ? window.innerHeight : "100vh" }}
+        >
           <Navbar
             open={isMenuOpen}
             onClick={() => setIsMenuOpen(!isMenuOpen)}
