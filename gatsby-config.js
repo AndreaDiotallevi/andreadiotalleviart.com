576473675e3273e2ee1c96efa6a2416120619641
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2021-10-23 16:14:28 +0100

    update packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,58 +1,64 @@
 require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
+    path: `.env.${process.env.NODE_ENV}`,
 })
 
 module.exports = {
-  siteMetadata: {
-    title: "Andrea Diotallevi | Generative Artist and Creative Coder",
-    author: "Andrea Diotallevi",
-    description:
-      "The artwork of Andrea Diotallevi, a practising generative artist, creative coder, software engineer, architect and pianist.",
-    url: "https://www.andreadiotalleviart.com",
-    image: "/moonlight.png",
-    twitterUsername: "@a_diotallevi_",
-  },
-  plugins: [
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sass",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "src",
-        path: `${__dirname}/src/`,
-      },
+    siteMetadata: {
+        title: "Andrea Diotallevi | Generative Artist and Creative Coder",
+        author: "Andrea Diotallevi",
+        description:
+            "The artwork of Andrea Diotallevi, a practising generative artist, creative coder, software engineer, architect and pianist.",
+        url: "https://www.andreadiotalleviart.com",
+        image: "/moonlight.png",
+        twitterUsername: "@a_diotallevi_",
     },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    "gatsby-transformer-json",
-    {
-      resolve: "gatsby-transformer-remark",
-      options: {
-        plugins: [
-          "gatsby-remark-reading-time",
-          "gatsby-remark-relative-images",
-          {
-            resolve: "gatsby-remark-images",
+    plugins: [
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sass",
+        {
+            resolve: "gatsby-source-filesystem",
             options: {
-              maxWidth: 750,
-              linkImagesToOriginal: false,
+                name: "src",
+                path: `${__dirname}/src/`,
+                cssLoaderOptions: {
+                    esModule: false,
+                    modules: {
+                        namedExport: false,
+                    },
+                },
             },
-          },
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: ["Work Sans"],
-        display: "swap",
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: process.env.GA_TRACKING_ID,
-      },
-    },
-  ],
+        },
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        "gatsby-transformer-json",
+        {
+            resolve: "gatsby-transformer-remark",
+            options: {
+                plugins: [
+                    // "gatsby-remark-reading-time",
+                    "gatsby-remark-relative-images",
+                    {
+                        resolve: "gatsby-remark-images",
+                        options: {
+                            maxWidth: 750,
+                            linkImagesToOriginal: false,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-fonts`,
+            options: {
+                fonts: ["Work Sans"],
+                display: "swap",
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: process.env.GA_TRACKING_ID,
+            },
+        },
+    ],
 }
