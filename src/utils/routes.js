f63841afb717cb40e919b436b882bfb347200d85
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2022-02-27 16:44:47 +0000

    add blog link

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,9 +1,10 @@
 const routes = [
     {
-        menuName: "Generative Art",
+        menuName: "Art",
         baseUrl: "/portfolio",
         pageTitle: "Generative Artist and Creative Coder",
     },
+    { menuName: "Blog", baseUrl: "/blog", pageTitle: "Blog" },
     { menuName: "Contact", baseUrl: "/contact", pageTitle: "About Me" },
     {
         menuName: "Home",
