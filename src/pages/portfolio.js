c86f8bd32513031c55b0a01a61d9bc4f98ca80f8
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2022-03-05 11:16:02 +0000

    amend portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -5,6 +5,7 @@ import Img from "gatsby-image"
 
 import Layout from "../templates/layout"
 import Seo from "../components/seo"
+import PageTitle from "../components/pageTitle"
 
 import * as portfolioStyles from "./portfolio.module.scss"
 
@@ -35,20 +36,32 @@ const Portfolio = props => {
                 title="Portfolio | Andrea Diotallevi"
                 description="The artwork of Andrea Diotallevi, a practising generative artist, creative coder, software engineer, architect and pianist"
             />
-            <div className={portfolioStyles.container}>
-                <ul>
-                    {props.data.allArtworksJson.edges.map(({ node }) => (
-                        <li key={node.name}>
-                            <Link to={`/portfolio/${node.slug}`}>
-                                <Img
-                                    fluid={node.images[0].childImageSharp.fluid}
-                                    className={portfolioStyles.image}
-                                />
-                                <p>{node.name}</p>
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    width: "100%",
+                    marginBottom: "50px",
+                }}
+            >
+                <PageTitle isHome={false} text="Generative Art" />
+                <div>
+                    <ul className={portfolioStyles.container}>
+                        {props.data.allArtworksJson.edges.map(({ node }) => (
+                            <li key={node.name}>
+                                <Link to={`/portfolio/${node.slug}`}>
+                                    <Img
+                                        fluid={
+                                            node.images[0].childImageSharp.fluid
+                                        }
+                                    />
+                                    <h2>{node.name}</h2>
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </Layout>
     )
