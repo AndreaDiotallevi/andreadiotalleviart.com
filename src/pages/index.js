3bae251597215f0f1fd00b84bef8b588e7eac568
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2022-03-05 11:16:55 +0000

    amend home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,24 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../templates/layout"
 import Moonlight from "../components/moonlight"
+import PageTitle from "../components/pageTitle"
 import Seo from "../components/seo"
 
-import "../styles/index.css"
+import "../styles/index2.scss"
+
+export const query = graphql`
+    query {
+        fileName: file(relativePath: { eq: "assets/test.png" }) {
+            childImageSharp {
+                fluid(maxWidth: 660) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+    }
+`
 
 const Home = () => {
     return (
@@ -13,7 +27,19 @@ const Home = () => {
                 title="Andrea Diotallevi | Generative Artist and Creative Coder"
                 description="The artwork of Andrea Diotallevi, a practising generative artist, creative coder, software engineer, architect and pianist"
             />
-            <Moonlight />
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    width: "100%",
+                }}
+            >
+                <PageTitle
+                    isHome={true}
+                    text="Generative Artist & Creative Coder"
+                />
+                <Moonlight />
+            </div>
         </Layout>
     )
 }
