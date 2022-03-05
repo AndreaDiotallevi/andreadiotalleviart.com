0b19de76eba547e3ecd5098059b4442288c8e65f
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2022-03-05 11:13:13 +0000

    amend Navbar component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,44 +6,28 @@ import routes from "../utils/routes"
 
 import * as navbarStyles from "./navbar.module.scss"
 
-const Navbar = ({ open, onClick, isHome }) => {
-    return (
-        <header
-            className={`${navbarStyles.container} ${
-                isHome ? navbarStyles.isHome : ""
-            }`}
-        >
-            <nav className={navbarStyles.navbar}>
-                <div>
-                    <Link to="/">
-                        <p className={`${isHome ? navbarStyles.isHome : ""}`}>
-                            Andrea Diotallevi
-                        </p>
-                    </Link>
-                </div>
-                <ul>
-                    {routes.map(({ menuName, baseUrl }) => (
-                        <li key={menuName}>
-                            <Link
-                                to={baseUrl}
-                                className={`${
-                                    isHome ? navbarStyles.isHome : ""
-                                }`}
-                                activeClassName={navbarStyles.activeNavItem}
-                            >
-                                {menuName}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-                <MenuToggleButton
-                    open={open}
-                    onClick={onClick}
-                    isHome={isHome}
-                />
-            </nav>
-        </header>
-    )
-}
+const Navbar = ({ open, onClick, isHome }) => (
+    <header
+        className={`${navbarStyles.header} ${
+            isHome ? navbarStyles.homepage : ""
+        }`}
+    >
+        <div>
+            <Link to="/">
+                <h2>Andrea Diotallevi</h2>
+            </Link>
+            <ul>
+                {routes.map(({ menuName, baseUrl }) => (
+                    <li key={menuName}>
+                        <Link to={baseUrl} activeStyle={{ fontWeight: 600 }}>
+                            {menuName}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+            <MenuToggleButton open={open} onClick={onClick} isHome={isHome} />
+        </div>
+    </header>
+)
 
 export default Navbar
