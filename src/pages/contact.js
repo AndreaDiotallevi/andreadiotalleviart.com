b2bea8199948c6aaf984c61c45c08d803b8c1041
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2022-03-05 11:16:18 +0000

    amend contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,6 +4,7 @@ import Img from "gatsby-image"
 
 import Layout from "../templates/layout"
 import Seo from "../components/seo"
+import PageTitle from "../components/pageTitle"
 
 import * as contactStyles from "./contact.module.scss"
 
@@ -26,34 +27,48 @@ const Contact = props => {
                 title="Contact | Andrea Diotallevi"
                 description="Get in touch if you are interested in working together"
             />
-            <div className={contactStyles.container}>
-                <div>
-                    <div className={contactStyles.image}>
-                        <Img
-                            fluid={props.data.fileName.childImageSharp.fluid}
-                        />
-                    </div>
-                    <div className={contactStyles.text}>
-                        <p>
-                            I am a generative artist and creative coder based in
-                            London.
-                        </p>
-                        <p>
-                            As a pianist and former architect, I am fascinated
-                            by the intersection between art and technology and I
-                            am in constant exploration of generative art
-                            concepts. For each new work, I design a custom
-                            algorithm capable of generating a sequence of
-                            unique, but aesthetically related images.
-                        </p>
-                        <p>------</p>
-                        <p>Email</p>
-                        <a
-                            className={contactStyles.email}
-                            href="mailto: andrea.diotallevi@outlook.com"
-                        >
-                            andrea.diotallevi@outlook.com
-                        </a>
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    width: "100%",
+                    marginBottom: "50px",
+                }}
+            >
+                <PageTitle isHome={false} text="Contact" />
+                <div className={contactStyles.container}>
+                    <div>
+                        <div className={contactStyles.image}>
+                            <Img
+                                fluid={
+                                    props.data.fileName.childImageSharp.fluid
+                                }
+                            />
+                        </div>
+                        <div className={contactStyles.text}>
+                            <p>
+                                I am a generative artist and creative coder
+                                based in London.
+                            </p>
+                            <p>
+                                As a pianist and former architect, I am
+                                fascinated by the intersection between art and
+                                technology and I am in constant exploration of
+                                generative art concepts. For each new work, I
+                                design a custom algorithm capable of generating
+                                a sequence of unique, but aesthetically related
+                                images.
+                            </p>
+                            <p>------</p>
+                            <p>Email</p>
+                            <a
+                                className={contactStyles.email}
+                                href="mailto: andrea.diotallevi@outlook.com"
+                            >
+                                andrea.diotallevi@outlook.com
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
