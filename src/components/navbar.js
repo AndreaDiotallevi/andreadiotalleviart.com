5e54bacdcd0ef3e4f97dbfd514aae5961c61633d
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2022-03-05 13:04:39 +0000

    improve styling

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -19,7 +19,7 @@ const Navbar = ({ open, onClick, isHome }) => (
             <ul>
                 {routes.map(({ menuName, baseUrl }) => (
                     <li key={menuName}>
-                        <Link to={baseUrl} activeStyle={{ opacity: 0.75 }}>
+                        <Link to={baseUrl} activeStyle={{ opacity: 0.7 }}>
                             {menuName}
                         </Link>
                     </li>
