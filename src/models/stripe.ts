e331d2ce47ef8b1b0bd7c4950a0a0ee2f040a6b1
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2023-12-17 18:40:27 +0100

    chore: create stripe models and fragments

---
+++ src/models/stripe.ts
@@ -0,0 +1,61 @@
+import { graphql } from "gatsby"
+import { IGatsbyImageData } from "gatsby-plugin-image"
+
+export type StripePrice = {
+    id: string
+    active: boolean
+    currency: string
+    unit_amount: number
+    product: {
+        id: string
+        name: string
+        description: string
+        metadata: {
+            slug: string
+            medium: string
+            dimensions: string
+        }
+        localFiles: [
+            {
+                childImageSharp: {
+                    gatsbyImageData: IGatsbyImageData
+                    fixed: {
+                        src: string
+                    }
+                }
+            }
+        ]
+    }
+}
+
+export const query = graphql`
+    fragment StripePriceFragment on StripePrice {
+        id
+        active
+        currency
+        unit_amount
+        product {
+            id
+            name
+            description
+            metadata {
+                slug
+                medium
+                dimensions
+            }
+            localFiles {
+                childImageSharp {
+                    gatsbyImageData(
+                        width: 660
+                        quality: 99
+                        layout: CONSTRAINED
+                        placeholder: BLURRED
+                    )
+                    fixed {
+                        src
+                    }
+                }
+            }
+        }
+    }
+`
