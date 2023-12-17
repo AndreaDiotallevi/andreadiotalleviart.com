eea5899d35bf596ca02f6f76b2a09a2f0b244898
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2023-12-17 18:42:38 +0100

    feat: throw error is env variable is not set

--- src/utils/stripejs.ts
+++ src/utils/stripejs.ts
@@ -6,11 +6,16 @@ import { loadStripe, Stripe } from "@stripe/stripe-js"
 let stripePromise: Promise<Stripe | null>
 
 const getStripe = () => {
-    if (!stripePromise) {
-        stripePromise = loadStripe(
-            process.env.GATSBY_STRIPE_PUBLISHABLE_KEY || ""
+    if (!process.env.GATSBY_STRIPE_PUBLISHABLE_KEY) {
+        throw new Error(
+            "GATSBY_STRIPE_PUBLISHABLE_KEY environment variable is undefined"
         )
     }
+
+    if (!stripePromise) {
+        stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY)
+    }
+
     return stripePromise
 }
 
