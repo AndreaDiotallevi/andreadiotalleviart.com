a7298b1c4bb4f9a8e2cec89112edf3c269e06c17
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-01-14 17:32:48 +0000

    feat: create function

---
+++ serverless/src/handlers/createCheckoutSession.ts
@@ -0,0 +1,31 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda"
+import Stripe from "stripe"
+
+import { createCheckoutSession } from "../data"
+
+export const handler = async (
+    event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+    const { line_items, cancel_url, success_url } = JSON.parse(
+        event.body as string
+    ) as Stripe.Checkout.SessionCreateParams
+
+    const { session, error } = await createCheckoutSession({
+        line_items,
+        cancel_url,
+        success_url,
+    })
+
+    const statusCode = error ? 500 : 200
+    const body = error ? JSON.stringify({ error }) : JSON.stringify({ session })
+
+    return {
+        statusCode,
+        body,
+        headers: {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET",
+        },
+    }
+}
