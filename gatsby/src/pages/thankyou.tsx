80e63060ae4943ff822aaf5be3b39a9bcf6ef868
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-01-21 16:02:48 +0000

    feat: make progress on thank you page

--- gatsby/src/pages/thankyou.tsx
+++ gatsby/src/pages/thankyou.tsx
@@ -7,17 +7,59 @@ import * as styles from "./thankyou.module.scss"
 const ThankYou = () => {
     return (
         <Layout seo={{ title: "test", description: "", tags: [] }}>
-            <table>
+            <table className={styles.table}>
                 <tbody>
                     <tr>
                         <td colSpan={3} className={styles.title}>
-                            Thank You For Your Order!
+                            Thanks for your order Bob!
                         </td>
                     </tr>
                     <tr>
-                        <td>Alfreds Futterkiste</td>
-                        <td>Maria Anders</td>
-                        <td>Germany</td>
+                        <td colSpan={3}>
+                            As soon as your package is on its way, you will
+                            receive a delivery confirmation from me by email.
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colSpan={3} className={styles.header}>
+                            Delivery address
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colSpan={3}>
+                            Andrea Diotallevi<br></br>
+                            Line 1<br></br>
+                            Line 2<br></br>
+                            Postcode<br></br>
+                            London<br></br>
+                            UK
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colSpan={3} className={styles.header}>
+                            Your items
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colSpan={3}>
+                            Archival Pigment Print of Flames<br></br>
+                            Dimensions: 30x30cm<br></br>
+                            Quantity: 1
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colSpan={3} className={styles.header}>
+                            Payment summary
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Payment method: Debit / Credit Card<br></br>
+                            Subtotal: £99.00<br></br>
+                            Shipping fee: Free<br></br>
+                            Discounts: £0.00<br></br>
+                            Total: £99.00
+                        </td>
                     </tr>
                 </tbody>
             </table>
