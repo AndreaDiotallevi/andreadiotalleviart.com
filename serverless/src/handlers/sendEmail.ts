408ad8857da17e1dc872967de000d7c870fe22e2
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-01-22 16:41:45 +0000

    feat: add queue policy

--- serverless/src/handlers/sendEmail.ts
+++ serverless/src/handlers/sendEmail.ts
@@ -3,33 +3,22 @@ import { SQSEvent, SQSRecord } from "aws-lambda"
 import { sendEmail } from "../data"
 
 export const handler = async (event: SQSEvent): Promise<void> => {
-    console.log(event)
-    // for (const record of event.Records) {
+    try {
+        for (const record of event.Records) {
+            console.log(record)
+            const { error } = await sendEmail()
 
-    // }
-    // try {
-    //     for (const record of event.Records) {
-    //         // Process each SQS message
-    //         const { error } = await sendEmail()
-    //     }
-    // } catch (error) {
-    //     console.error("Error processing SQS event:", error)
-    //     throw error // Let Lambda retry the event
-    // }
+            if (error) {
+                console.log("Error processing SQS event record")
+                console.error(error)
 
-    // const { error } = await sendEmail()
+                throw new Error("Could not process SQS event record")
+            }
+        }
+    } catch (error) {
+        console.log("Error processing SQS event")
+        console.error(error)
 
-    // const statusCode = error ? 500 : 200
-
-    // const body = error ? JSON.stringify({ error }) : JSON.stringify("OK")
-
-    // return {
-    //     statusCode,
-    //     body,
-    //     headers: {
-    //         "Access-Control-Allow-Headers": "Content-Type, X-Api-Key",
-    //         "Access-Control-Allow-Origin": "*",
-    //         "Access-Control-Allow-Methods": "*",
-    //     },
-    // }
+        throw new Error("Could not process SQS event")
+    }
 }
