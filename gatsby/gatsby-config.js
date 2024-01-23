2517f3d35d6570f2bba64965dd9e925abaeaeba5
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-01-23 10:29:40 +0000

    chore: remove old UA analytics tracking id

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -56,10 +56,7 @@ module.exports = {
         {
             resolve: `gatsby-plugin-google-gtag`,
             options: {
-                trackingIds: [
-                    process.env.GA_TRACKING_ID,
-                    process.env.GA4_TRACKING_ID,
-                ],
+                trackingIds: [process.env.GA4_TRACKING_ID],
                 pluginConfig: {
                     head: true,
                 },
