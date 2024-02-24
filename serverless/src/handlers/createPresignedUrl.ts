f0759b832a8669877c820710fed590526e42681d
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-02-24 19:27:24 +0000

    refactor: add send email step to state machine

--- serverless/src/handlers/createPresignedUrl.ts
+++ serverless/src/handlers/createPresignedUrl.ts
@@ -1,10 +1,16 @@
+import Stripe from "stripe"
+
 import { createPresignedUrl } from "../data"
 
 export type CreatePresignedUrlResponse = {
     url: string
 }
 
-export const handler = async (): Promise<CreatePresignedUrlResponse> => {
+export const handler = async (event: {
+    detail: Stripe.Event
+}): Promise<CreatePresignedUrlResponse> => {
+    console.log(event.detail)
+
     const { url } = await createPresignedUrl()
 
     return {
--- serverless/src/handlers/sendEmail.ts
+++ serverless/src/handlers/sendEmail.ts
@@ -1,34 +1,26 @@
-import { SQSEvent, SQSRecord } from "aws-lambda"
-
 import Stripe from "stripe"
 
 import { sendEmail } from "../data"
 
-export const handler = async (event: SQSEvent): Promise<void> => {
+export const handler = async (event: {
+    detail: Stripe.Event
+}): Promise<void> => {
     try {
-        for (const record of event.Records) {
-            const body = JSON.parse(record.body)
+        // if (stripeEvent.type !== 'checkout.session.completed') {
+        //     // Do something
+        // }
 
-            const stripeEvent = body.detail as Stripe.Event
+        const sessionId = event.detail.data.object.id
+        const { error } = await sendEmail({ sessionId })
 
-            // if (stripeEvent.type !== 'checkout.session.completed') {
-            //     // Do something
-            // }
+        if (error) {
+            console.error(error)
 
-            const sessionId = stripeEvent.data.object.id
-            const { error } = await sendEmail({ sessionId })
-
-            if (error) {
-                console.log("Error processing SQS event record")
-                console.error(error)
-
-                throw new Error("Could not process SQS event record")
-            }
+            throw new Error("Could not process SQS event record")
         }
     } catch (error) {
-        console.log("Error processing SQS event")
         console.error(error)
 
-        throw new Error("Could not process SQS event")
+        throw error
     }
 }
