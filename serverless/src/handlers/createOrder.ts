e84273b45040b6c2a64bb533442d7d8ec3979cf1
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-03-29 17:59:05 +0000

    infra: add queue in front of create order function

--- serverless/src/handlers/createOrder.ts
+++ serverless/src/handlers/createOrder.ts
@@ -1,26 +1,27 @@
-import { EventBridgeEvent } from "aws-lambda"
+import { SQSEvent } from "aws-lambda"
 import Stripe from "stripe"
 
 import { createOrder } from "../services/prodigi"
 import { retrieveCheckoutSession } from "../services/stripe"
 
-export const handler = async (
-    event: EventBridgeEvent<
-        "CheckoutSessionCompleted",
-        Stripe.CheckoutSessionCompletedEvent
-    >
-) => {
-    const { session } = await retrieveCheckoutSession({
-        sessionId: event.detail.data.object.id,
-    })
+export const handler = async (event: SQSEvent): Promise<void> => {
+    try {
+        for (const record of event.Records) {
+            const body = JSON.parse(record.body)
+            const event = body.detail as Stripe.CheckoutSessionCompletedEvent
+            const sessionId = event.data.object.id
+            const { session } = await retrieveCheckoutSession({ sessionId })
 
-    console.log(session.customer_details)
-    console.log(session.shipping_details)
-    console.log(session.line_items?.data)
+            console.log(JSON.stringify(session))
 
-    await createOrder({
-        customerDetails: session.customer_details,
-        shippingDetails: session.shipping_details,
-        lineItems: session.line_items,
-    })
+            await createOrder({
+                customerDetails: session.customer_details,
+                shippingDetails: session.shipping_details,
+                lineItems: session.line_items,
+            })
+        }
+    } catch (error) {
+        console.error(error)
+        throw error
+    }
 }
