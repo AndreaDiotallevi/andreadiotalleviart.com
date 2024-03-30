7e81461a7ee4f95e5ea7fb0c1951538c18f52f08
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-03-30 09:43:57 +0000

    feat: send trackingUrl param

--- serverless/src/handlers/sendOrderShippedEmail.ts
+++ serverless/src/handlers/sendOrderShippedEmail.ts
@@ -24,7 +24,10 @@ export const handler = async (event: SQSEvent): Promise<void> => {
                     ToAddresses: [emailDestination],
                 },
                 Template: "SendOrderShippedEmailTemplate",
-                TemplateData: JSON.stringify({ name: "test" }),
+                TemplateData: JSON.stringify({
+                    name: event.data.order.recipient.name,
+                    trackingUrl: event.data.order.shipments[0].tracking.url,
+                }),
                 // Template: "CheckoutSessionCompletedEmailTemplate",
                 // TemplateData: JSON.stringify({
                 //     name: session.customer_details?.name || "",
