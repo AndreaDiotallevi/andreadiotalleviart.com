2aec0bc53bf14e8350fac8ceb6bb8c7a205c2d5c
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-04-21 19:46:12 +0100

    feat: create input components

---
+++ gatsby/src/components/inputField.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+
+import * as styles from "./inputField.module.scss"
+
+const InputField = ({
+    value,
+    name,
+    label,
+    onChange,
+    error,
+}: {
+    value: string
+    name: string
+    label?: string
+    onChange: (value: string) => void
+    error?: string
+}) => {
+    return (
+        <div className={styles.container}>
+            {label ? <label className={styles.label}>{label}</label> : null}
+            <input
+                className={styles.input}
+                name={name}
+                value={value}
+                onChange={event => onChange(event.target.value)}
+            />
+            {error ? <p className={styles.error}>{error}</p> : null}
+        </div>
+    )
+}
+
+export default InputField
