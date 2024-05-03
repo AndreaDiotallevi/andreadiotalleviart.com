ef8e49a503daef629dfacecb6aa42f08b64821e3
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-05-03 11:48:11 +0100

    feat: generate sitemap

--- gatsby/src/components/seo.tsx
+++ gatsby/src/components/seo.tsx
@@ -93,7 +93,7 @@ const query = graphql`
             siteMetadata {
                 defaultTitle: title
                 defaultDescription: description
-                siteUrl: url
+                siteUrl
                 defaultImage: image
                 twitterUsername
             }
