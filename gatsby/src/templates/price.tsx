22bf23e1563ca19b6f1b10c7528b732dd20c3442
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-05-10 21:51:34 +0100

    feat: amend page title

--- gatsby/src/templates/price.tsx
+++ gatsby/src/templates/price.tsx
@@ -206,7 +206,7 @@ export const query = graphql`
 
 export const Head = ({ data: { allStripePrice } }: PageProps<DataProps>) => (
     <Seo
-        title={`${allStripePrice.edges[0].node.product.metadata.displayName} | Giclée Fine Art Prints | Andrea Diotallevi Art`}
+        title={`${allStripePrice.edges[0].node.product.metadata.displayName} | Giclée Fine Art Print | Andrea Diotallevi Art`}
         description={allStripePrice.edges[0].node.product.description}
         image={allStripePrice.edges[0].node.mockup.childImageSharp.original.src}
         type="product"
