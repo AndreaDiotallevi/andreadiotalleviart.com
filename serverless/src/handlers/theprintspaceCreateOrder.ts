c935a3f1b95201a416a305cc50894ec87fcfaf93
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-08-23 14:33:21 +0300

    refactor: improve typing

--- serverless/src/handlers/theprintspaceCreateOrder.ts
+++ serverless/src/handlers/theprintspaceCreateOrder.ts
@@ -1,35 +1,33 @@
-import { SQSEvent } from "aws-lambda"
+import { SQSHandler } from "aws-lambda"
 import Stripe from "stripe"
 
+import { retrieveCheckoutSession } from "../services/stripe"
 import {
     createConfirmedOrder,
     createEmbryonicOrder,
 } from "../services/theprintspace"
-import { retrieveCheckoutSession } from "../services/stripe"
 
-export const handler = async (event: SQSEvent): Promise<void> => {
-    try {
-        for (const record of event.Records) {
-            const body = JSON.parse(record.body)
-            const event = body.detail as Stripe.CheckoutSessionCompletedEvent
-            const sessionId = event.data.object.id
-            const { session } = await retrieveCheckoutSession({ sessionId })
-
-            console.log(JSON.stringify(session))
-
-            if (process.env.ENVIRONMENT !== "production") {
-                // Required until Creativehub updates the SSL certificate for the sandbox API environment (which has expired)
-                process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
-            }
-
-            const { orderId, deliveryOptions } = await createEmbryonicOrder({
-                session,
-            })
-
-            await createConfirmedOrder({ orderId, deliveryOptions })
+export const handler: SQSHandler = async (event): Promise<void> => {
+    for (const record of event.Records) {
+        const body = JSON.parse(record.body) as {
+            detail: Stripe.CheckoutSessionCompletedEvent
         }
-    } catch (error) {
-        console.error(error)
-        throw error
+
+        const event = body.detail
+        const sessionId = event.data.object.id
+        const { session } = await retrieveCheckoutSession({ sessionId })
+
+        console.log(JSON.stringify(session))
+
+        if (process.env.ENVIRONMENT !== "production") {
+            // Required until Creativehub updates the SSL certificate for the sandbox API environment (which has expired)
+            process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+        }
+
+        const { orderId, deliveryOptions } = await createEmbryonicOrder({
+            session,
+        })
+
+        await createConfirmedOrder({ orderId, deliveryOptions })
     }
 }
