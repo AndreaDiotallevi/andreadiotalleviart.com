37ccc22037d951ca64eb99831b498f2f6405d399
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-09-07 21:45:28 +0100

    feat: make site fully SSG

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -2,11 +2,8 @@ import { defineConfig } from "astro/config"
 
 import tailwind from "@astrojs/tailwind"
 
-import netlify from "@astrojs/netlify";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: "server",
-  adapter: netlify(),
+    integrations: [tailwind()],
+    output: "static",
 })
