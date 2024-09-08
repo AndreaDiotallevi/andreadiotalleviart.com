6b9b0d3f4ae190245876f021d363a288abcc658c
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-09-08 17:44:14 +0100

    feat: add success page

--- astro/src/utils/stripe.ts
+++ astro/src/utils/stripe.ts
@@ -90,7 +90,7 @@ export const createCheckoutSession = async (params: {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
-                    "X-Api-Key": import.meta.env.PUBLIC_API_KEY!,
+                    "X-Api-Key": import.meta.env.PUBLIC_API_KEY,
                 },
                 body: JSON.stringify(params),
             },
@@ -103,6 +103,44 @@ export const createCheckoutSession = async (params: {
             return data
         } else {
             throw new Error("Failed to create checkout session")
+        }
+    } catch (error) {
+        console.error("Error during request: ", error)
+        return null
+    }
+}
+
+export const retrieveCheckoutSession = async (params: {
+    sessionId: string
+}) => {
+    if (!import.meta.env.PUBLIC_API_KEY) {
+        throw new Error("The api key is undefined.")
+    }
+
+    try {
+        const response = await fetch(
+            import.meta.env.PUBLIC_API_URL +
+                `/stripe-retrieve-checkout-session/${params.sessionId}`,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-Api-Key": import.meta.env.PUBLIC_API_KEY,
+                },
+            },
+        )
+
+        if (response.ok) {
+            const data = (await response.json()) as {
+                session: Stripe.Checkout.Session
+            }
+            return data.session
+        } else {
+            console.error(
+                "Failed to retrieve checkout session: ",
+                response.statusText,
+            )
+            return null
         }
     } catch (error) {
         console.error("Error during request: ", error)
