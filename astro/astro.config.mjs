fdae145100af8fc6551d062ef5df145824550129
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-09-11 16:42:29 +0100

    feat: setup gtag

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from "astro/config"
 
+import partytown from "@astrojs/partytown"
 import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+    integrations: [
+        tailwind(),
+        partytown({
+            config: {
+                forward: ["dataLayer.push"],
+            },
+        }),
+    ],
     output: "static",
     prefetch: {
         defaultStrategy: "viewport",
