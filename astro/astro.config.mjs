a560083319b11c1183a8545dab8e088819cc4257
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-09-17 12:44:41 +0100

    feat: add view transitions and update scripts

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config"
 import { loadEnv } from "vite"
 
-import partytown from "@astrojs/partytown"
+// import partytown from "@astrojs/partytown"
 import tailwind from "@astrojs/tailwind"
 
 const { IMAGES_DOMAIN } = loadEnv(process.env.NODE_ENV, process.cwd(), "")
@@ -12,11 +12,11 @@ export default defineConfig({
     site: "https://www.andreadiotalleviart.com",
     integrations: [
         tailwind(),
-        partytown({
-            config: {
-                forward: ["dataLayer.push"],
-            },
-        }),
+        // partytown({
+        //     config: {
+        //         forward: ["dataLayer.push"],
+        //     },
+        // }),
     ],
     output: "static",
     prefetch: {
