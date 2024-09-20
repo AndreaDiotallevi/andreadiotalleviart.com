a1e023b76f49cdd908ed9801a8c79a4d26dbaa07
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-09-20 16:20:15 +0100

    feat: add netlify locals types

--- astro/src/env.d.ts
+++ astro/src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference path="../.astro/types.d.ts" />
+
+type NetlifyLocals = import("@astrojs/netlify").NetlifyLocals
+
+declare namespace App {
+    interface Locals extends NetlifyLocals {
+        // ...
+    }
+}
