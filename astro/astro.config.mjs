99e9d3b2d752d80bca166640b6359e1d2d130a8b
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-10-05 15:06:16 +0100

    feat: add sitemap

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from "astro/config"
 import { imageService } from "@unpic/astro/service"
-
 import tailwind from "@astrojs/tailwind"
-
 import netlify from "@astrojs/netlify"
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,7 +10,12 @@ export default defineConfig({
     adapter: netlify(),
     trailingSlash: "never",
     build: { format: "file" },
-    integrations: [tailwind()],
+    integrations: [
+        tailwind(),
+        sitemap({
+            filter: page => !page.includes("/shop"),
+        }),
+    ],
     experimental: { serverIslands: true },
     prefetch: { defaultStrategy: "load", prefetchAll: true },
     site: "https://www.andreadiotalleviart.com",
