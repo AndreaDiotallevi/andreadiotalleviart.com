72cfaac06a52b6a6ec6e5e0adfd96de9f56cccac
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-10-25 18:42:03 +0100

    feat: add script for adding and removing cart items

---
+++ astro/src/scripts/cart.ts
@@ -0,0 +1,119 @@
+import type Stripe from "stripe"
+
+import {
+    createCheckoutSession,
+    retrieveCheckoutSession,
+} from "@utils/serverless"
+import {
+    getSessionId,
+    setOrUpdateCartTotalQuantity,
+    setOrUpdateSessionId,
+} from "@utils/localStorage"
+import { navigate } from "astro:transitions/client"
+import type { Currency } from "@utils/stripe"
+
+export const addToCart = async ({
+    priceId,
+    currency,
+}: {
+    priceId: string
+    currency: Currency
+}) => {
+    let lineItems: Stripe.Checkout.SessionCreateParams.LineItem[] = []
+
+    const sessionId = getSessionId()
+
+    if (sessionId) {
+        const currentSession = await retrieveCheckoutSession({
+            sessionId,
+        })
+
+        const currentLineItems = currentSession?.line_items?.data.map(item => ({
+            price: item.price?.id || "",
+            quantity: item.quantity || 1,
+        }))!
+
+        if (currentLineItems.find(item => item.price === priceId)) {
+            lineItems = currentLineItems.map(item => ({
+                price: item.price,
+                quantity:
+                    item.price === priceId ? item.quantity + 1 : item.quantity,
+            }))
+        } else {
+            lineItems = [...currentLineItems, { price: priceId, quantity: 1 }]
+        }
+    } else {
+        lineItems = [{ price: priceId, quantity: 1 }]
+    }
+
+    const session = await createCheckoutSession({
+        line_items: lineItems,
+        success_url: `${window.location.origin}/shop/checkout/success`,
+        currency,
+    })
+
+    if (!session) return { error: true }
+
+    setOrUpdateCartTotalQuantity({
+        quantity:
+            session.line_items?.data.reduce(
+                (total, item) => total + item.quantity!,
+                0,
+            ) || 0,
+    })
+    setOrUpdateSessionId({ sessionId: session.id })
+    navigate(`/cart?session_id=${session.id}`)
+}
+
+export const removeFromCart = async ({
+    priceId,
+    currency,
+}: {
+    priceId: string
+    currency: Currency
+}) => {
+    let lineItems: Stripe.Checkout.SessionCreateParams.LineItem[] = []
+
+    const sessionId = getSessionId()
+
+    if (sessionId) {
+        const currentSession = await retrieveCheckoutSession({
+            sessionId,
+        })
+
+        const currentLineItems = currentSession?.line_items?.data.map(item => ({
+            price: item.price?.id || "",
+            quantity: item.quantity || 1,
+        }))!
+
+        if (currentLineItems.find(item => item.price === priceId)) {
+            lineItems = currentLineItems.map(item => ({
+                price: item.price,
+                quantity:
+                    item.price === priceId ? item.quantity - 1 : item.quantity,
+            }))
+        } else {
+            return
+        }
+    } else {
+        return
+    }
+
+    const session = await createCheckoutSession({
+        line_items: lineItems.filter(item => item.quantity! > 0),
+        success_url: `${window.location.origin}/shop/checkout/success`,
+        currency,
+    })
+
+    if (!session) return
+
+    setOrUpdateCartTotalQuantity({
+        quantity:
+            session.line_items?.data.reduce(
+                (total, item) => total + item.quantity!,
+                0,
+            ) || 0,
+    })
+    setOrUpdateSessionId({ sessionId: session.id })
+    navigate(`/cart?session_id=${session.id}`)
+}
