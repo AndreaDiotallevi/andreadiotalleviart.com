3c6d4e96271b5709072e592f01b9c11bd36df8da
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-10-26 13:01:28 +0100

    chore: remove old gatsby site codebase

--- gatsby/src/global.d.ts
+++
@@ -1,5 +0,0 @@
-// necessary to make scss module work. See https://github.com/gatsbyjs/gatsby/issues/8144#issuecomment-438206866
-declare module "*.scss" {
-    const content: { [className: string]: string }
-    export = content
-}
