736a8610f8645d24efb2e4715e8fc0c513a71086
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-11-15 12:19:52 +0000

    feat: send email with promotion code

--- serverless/src/lambdas/api_newsletterCreateContact.ts
+++ serverless/src/lambdas/api_newsletterCreateContact.ts
@@ -1,12 +1,31 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda"
 
 import { newsletterCreateContact } from "../actions/ses_createContact"
+import { sendEmail } from "../actions/ses_sendEmail"
+import { getParameterValue } from "../actions/ssm_getParameterValue"
 
 export const handler = async (
     event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
     const { email } = JSON.parse(event.body as string) as { email: string }
     const { error } = await newsletterCreateContact({ email })
+
+    const emailSource = await getParameterValue<string>({
+        name: "EMAIL_ANDREA",
+    })
+
+    const promotionCode = await getParameterValue<string>({
+        name: "NEWSLETTER_PROMOTION_CODE",
+    })
+
+    await sendEmail({
+        Source: emailSource,
+        Destination: {
+            ToAddresses: [email],
+        },
+        Template: "NewsletterWelcomeEmailTemplate",
+        TemplateData: JSON.stringify({ promotionCode }),
+    })
 
     const statusCode = error ? 500 : 200
     const body = error
