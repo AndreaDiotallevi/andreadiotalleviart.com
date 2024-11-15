8c7b375dcf3290eda378fed73a3d7aecb1b7c109
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-11-15 12:30:37 +0000

    feat: add log

--- serverless/src/actions/ses_sendEmail.ts
+++ serverless/src/actions/ses_sendEmail.ts
@@ -8,6 +8,7 @@ const sesClient = new SESClient({ region: process.env.AWS_REGION })
 
 export const sendEmail = async (input: SendTemplatedEmailCommandInput) => {
     try {
+        console.log("Sending email...")
         const sendEmailCommand = new SendTemplatedEmailCommand({
             ...input,
             Source: `Andrea Diotallevi Art <${input.Source}>`,
