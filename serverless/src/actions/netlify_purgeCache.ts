341cc301b4d52cb91b69211d5a227dbc25cdf809
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-11-18 19:43:33 +0000

    feat: purge Netlify cache with cache tag 'stripe' after synchronising products

---
+++ serverless/src/actions/netlify_purgeCache.ts
@@ -0,0 +1,20 @@
+import { purgeCache } from "@netlify/functions"
+import { getParameterValue } from "./ssm_getParameterValue"
+
+export const purgeNetlifyCache = async () => {
+    const token = await getParameterValue<string>({
+        name: "NETLIFY_ACCESS_TOKEN",
+        withDecryption: true,
+    })
+
+    const siteID = await getParameterValue<string>({
+        name: "NETLIFY_SITE_ID",
+        withDecryption: true,
+    })
+
+    console.log("Purging Netlify Cache...")
+
+    await purgeCache({ token, siteID, tags: ["stripe"] })
+
+    return new Response("Purged!", { status: 202 })
+}
--- serverless/src/lambdas/api_stripeSynchroniseProducts.ts
+++ serverless/src/lambdas/api_stripeSynchroniseProducts.ts
@@ -1,9 +1,12 @@
 import { APIGatewayProxyResult } from "aws-lambda"
 
 import { stripeSynchroniseProducts } from "../actions/stripe_synchroniseProducts"
+import { purgeNetlifyCache } from "../actions/netlify_purgeCache"
 
 export const handler = async (): Promise<APIGatewayProxyResult> => {
     await stripeSynchroniseProducts()
+
+    await purgeNetlifyCache()
 
     return {
         statusCode: 200,
