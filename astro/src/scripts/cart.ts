18a61bc2e334caee0b1951a12f4045eef0205438
Andrea Diotallevi <andrea.diotallevi@deliveroo.co.uk>
2025-11-04 21:13:02 +0000

    feat: revert back to /cart page

--- astro/src/scripts/cart.ts
+++ astro/src/scripts/cart.ts
@@ -42,8 +42,8 @@ export const addToCart = async ({
     if (!session) return { error }
 
     updateClientSession({ session, promotionCode })
-    navigate(`/${currency}/cart?session_id=${session.id}`, {
-        history: (window.location.pathname.endsWith("/cart") || /\/([a-z]{3})\/cart$/.test(window.location.pathname)) ? "replace" : "push",
+    navigate(`/cart?session_id=${session.id}`, {
+        history: window.location.pathname.endsWith("/cart") ? "replace" : "push",
     })
 }
 
@@ -88,7 +88,7 @@ export const removeFromCart = async ({
     if (!session) return
 
     updateClientSession({ session, promotionCode })
-    navigate(`/${currency}/cart?session_id=${session.id}`, { history: "replace" })
+    navigate(`/cart?session_id=${session.id}`, { history: "replace" })
 }
 
 export const addPromotionCode = async ({
